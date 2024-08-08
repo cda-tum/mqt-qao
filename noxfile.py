f43d09cf9bc743e512320dd45bfe255bba350b60
DeborahVolpe <s270156@studenti.polito.it>
2024-08-08 11:07:30 +0200

    Update workflow

--- noxfile.py
+++ noxfile.py
@@ -25,7 +25,7 @@ BUILD_REQUIREMENTS = [
     "cython>=3.0",
     "cppy>=1.1.0",
     "kiwisolver>=1.3.2",
-    "dimod>=0.12.16",
+    "dimod>=0.12",
     "numpy>=2.0.0",
     "setuptools_dso>=0.1.0",
 ]
